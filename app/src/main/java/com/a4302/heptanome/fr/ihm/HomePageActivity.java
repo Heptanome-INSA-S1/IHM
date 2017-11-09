a8652d8541ab52a4ecb0b277f8d60189f8da2750
TimGonnet <gonnet.tim@gmail.com>
2017-11-09 20:38:09 +0100

    + can close bottom dialog

--- app/src/main/java/com/a4302/heptanome/fr/ihm/HomePageActivity.java
+++ app/src/main/java/com/a4302/heptanome/fr/ihm/HomePageActivity.java
@@ -1,11 +1,9 @@
 package com.a4302.heptanome.fr.ihm;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 /**
@@ -13,25 +11,28 @@ import android.widget.LinearLayout;
  */
 
 public class HomePageActivity extends AppCompatActivity {
-    private LinearLayout ChooseProposeOrrequest;
+    private LinearLayout ChooseProposeOrRequest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         setContentView(R.layout.home_page);
 
-        ChooseProposeOrrequest=(LinearLayout)findViewById(R.id.choose_purpose_or_request);
+        ChooseProposeOrRequest =(LinearLayout)findViewById(R.id.choose_purpose_or_request);
         findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    ChooseProposeOrrequest.setVisibility(View.VISIBLE);
+                    if(ChooseProposeOrRequest.getVisibility()==View.VISIBLE)
+                        ChooseProposeOrRequest.setVisibility(View.GONE);
+                    else
+                        ChooseProposeOrRequest.setVisibility(View.VISIBLE);
                 }
             }
         );
         findViewById(R.id.cancel_request_button).setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View view) {
-                   ChooseProposeOrrequest.setVisibility(View.GONE);
+                   ChooseProposeOrRequest.setVisibility(View.GONE);
                }
            }
         );
