91167b670e6c22d30541c54149e0508a133b2073
TimGonnet <gonnet.tim@gmail.com>
2017-11-09 19:59:14 +0100

    Correct small bug

--- app/src/main/java/com/a4302/heptanome/fr/ihm/HomePageActivity.java
+++ app/src/main/java/com/a4302/heptanome/fr/ihm/HomePageActivity.java
@@ -2,6 +2,7 @@ package com.a4302.heptanome.fr.ihm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 /**
  * Created by lrouquette on 08/11/2017.
@@ -12,6 +13,7 @@ public class HomePageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         setContentView(R.layout.home_page);
     }
 
